b4c8cb976ddd195f5314a55ec2833290623672dc
yonlu <lss.emailbox@gmail.com>
2019-08-12 14:25:00 -0400

    style: stylized Sign in page

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,14 +1,48 @@
 import React from 'react';
+import { Image } from 'react-native';
+
+import logo from '~/assets/logo.png';
 
 import Input from '~/components/Input';
 import Button from '~/components/Button';
 import Background from '~/components/Background';
 
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
 export default function SignIn() {
   return (
     <Background>
-      <Input style={{ marginTop: 30 }} icon="call" placeholder="Your name" />
-      <Button>Sign In</Button>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Enter your email"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Enter your password"
+          />
+
+          <SubmitButton onPress={() => {}}>Sign In</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Create an account</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
