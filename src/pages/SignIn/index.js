f5a1fc8dccaa08a9ebc351a7c0cfc709a8ea05fb
yonlu <lss.emailbox@gmail.com>
2019-08-12 13:44:59 -0400

    feature: styled input and button components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Input style={{ marginTop: 30 }} icon="call" placeholder="Your name" />
+      <Button>Sign In</Button>
     </Background>
   );
 }
