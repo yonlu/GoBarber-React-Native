2e74794b849911596cb948d4463f229db0b0a0a5
yonlu <lss.emailbox@gmail.com>
2019-08-14 14:07:42 -0400

    style: added initial appointment styling

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Left, Avatar, Info, Name, Time } from './styles';
+
+export default function Appointment() {
+  return (
+    <Container>
+      <Left>
+        <Avatar
+          source={{ uri: 'https://api.adorable.io/avatar/50/rocketseat.png' }}
+        />
+
+        <Info>
+          <Name>Lucas Sallada</Name>
+          <Time>in 3 hours</Time>
+        </Info>
+      </Left>
+
+      <TouchableOpacity onPress={() => {}}>
+        <Icon name="event-busy" size={20} color="#f64c75" />
+      </TouchableOpacity>
+    </Container>
+  );
+}
