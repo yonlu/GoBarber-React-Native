800567faa9f1e2018cc97b59c94f499bd127d69b
yonlu <lss.emailbox@gmail.com>
2019-08-19 09:17:14 -0400

    feature: added provider list page

---
+++ src/pages/New/SelectProvider/styles.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+import { TouchableOpacity } from 'react-native';
+
+export const Container = styled.SafeAreaView``;
+
+export const ProvidersList = styled.FlatList.attrs({
+  showsVerticalScrollIndicator: false,
+  numColumns: 2,
+})`
+  margin-top: 60px;
+  padding: 0 20px;
+`;
+
+export const Provider = styled(TouchableOpacity)`
+  background: #fff;
+  border-radius: 5px;
+  padding: 20px;
+  flex: 1;
+
+  align-items: center;
+  margin: 0 10px 20px;
+`;
+
+export const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 25px;
+`;
+
+export const Name = styled.Text`
+  margin-top: 15px;
+  font-size: 14px;
+  font-weight: bold;
+  color: #333;
+  text-align: center;
+`;
